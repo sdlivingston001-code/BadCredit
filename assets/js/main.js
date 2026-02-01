f8ba65ac2fa8ce07f4512a3f65ed6006520e10d4
sdlivingston001-code <sdlivingston001@gmail.com>
2026-02-01 22:02:59 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
+import { loadTerritories, resolveTerritory } from './territoryEngine.js';
 
+const territories = await loadTerritories();
+
+const result = resolveTerritory(territories.gamblingDen);
+console.log(result);
