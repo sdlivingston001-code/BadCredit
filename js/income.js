70f2acfbc2e8828e3502be5f576d7faa85b4fba9
sdlivingston001-code <sdlivingston001@gmail.com>
2026-02-01 01:09:43 +0000

    Update income.js

--- js/income.js
+++ js/income.js
@@ -1,78 +1,24 @@
 // -----------------------------
-// Dice Helpers
-// -----------------------------
-
-function rollD6() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
-function rollND6(n) {
-  let rolls = [];
-  for (let i = 0; i < n; i++) {
-    rolls.push(rollD6());
-  }
-  return rolls;
-}
-
-// -----------------------------
 // Territory Rules
 // -----------------------------
 
-const territoryRules = {
-  "Settlement": () => {
-    // Income roll
-    const incomeRoll = rollD6();
-    const income = incomeRoll * 10;
+import { settlementRule } from "./territories/settlement.js";
 
-    // Recruitment roll (2D6)
-    const recruitRolls = rollND6(2);
-    const sixes = recruitRolls.filter(r => r === 6).length;
+// -----------------------------
+// Dice Helpers
+// -----------------------------
 
-    let recruitment = "No new recruits";
+function rollDie(sides) {
+  return Math.floor(Math.random() * sides) + 1;
+}
 
-    if (sixes === 2) {
-      recruitment = "Recruit a ganger OR two juves";
-    } else if (sixes === 1) {
-      recruitment = "Recruit a juve";
-    }
-
-    return {
-      type: "Settlement",
-      incomeRoll,
-      income,
-      recruitRolls,
-      recruitment
-    };
-  },
-
-  "Gambling Den": () => {
-    return {
-      type: "Gambling Den",
-      income: rollD6()
-    };
-  },
-
-  "Mine Workings": () => {
-    return {
-      type: "Mine Workings",
-      income: rollD6() * 10
-    };
-  },
-
-  "Generatorium": () => {
-    return {
-      type: "Generatorium",
-      income: rollD6() * 15
-    };
-  },
-
-  "Slag Furnace": () => {
-    return {
-      type: "Slag Furnace",
-      income: rollD6() * 20
-    };
+function rollNDice(n, sides) {
+  let rolls = [];
+  for (let i = 0; i < n; i++) {
+    rolls.push(rollDie(sides));
   }
-};
+  return rolls;
+}
 
 // -----------------------------
 // Main Generator Logic
