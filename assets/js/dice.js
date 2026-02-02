b89eaa50ac0d434792d25e0bea99cd0a54bbb0f0
sdlivingston001-code <sdlivingston001@gmail.com>
2026-02-02 17:31:42 +0000

    Update dice.js

--- assets/js/dice.js
+++ assets/js/dice.js
@@ -12,41 +12,48 @@ const Dice = {
 
   d66() {
     return this.d6()*10 + this.d6();
-  }
+  },
 
-  firepower() {
+  d6_firepower() {
     const table = {
-      1: { shots: 1, jam: false, label: "Single shot" },
-      2: { shots: 2, jam: false, label: "Sustained fire (2)" },
-      3: { shots: 3, jam: false, label: "Sustained fire (3)" },
-      4: { shots: 1, jam: true,  label: "Ammo check!" },
-      5: { shots: 1, jam: false, label: "Single shot" },
-      6: { shots: 2, jam: false, label: "Sustained fire (2)" }
+      1: { shots: 1, jam: false, label: "One hit"},
+      2: { shots: 1, jam: false, label: "One hit"},
+      3: { shots: 2, jam: false, label: "Two hits" },
+      4: { shots: 2, jam: false, label: "Two hits" },
+      5: { shots: 3, jam: false, label: "Three hits" },
+      6: { shots: 1, jam: true, label: "One hit and Ammo check" }
     };
 
     const roll = this.d6();
     return { roll, ...table[roll] };
   },
 
-  injury() {
+  d6_injury() {
     const table = {
-      1: { result: "Flesh wound", effect: -1 },
-      2: { result: "Flesh wound", effect: -1 },
-      3: { result: "Serious injury", effect: null },
-      4: { result: "Serious injury", effect: null },
-      5: { result: "Out of action", effect: null },
-      6: { result: "Out of action", effect: null }
+      1: { result: "Flesh wound"},
+      2: { result: "Flesh wound"},
+      3: { result: "Serious injury"},
+      4: { result: "Serious injury"},
+      5: { result: "Serious injury"},
+      6: { result: "Out of action"}
     };
 
     const roll = this.d6();
     return { roll, ...table[roll] };
-  }
- };
-  
-  rollMany(n, sides = 6) {
+  },
+
+  //Example usage:
+  // Dice.rollMany(3); // rolls three d6 by default
+  // Dice.rollMany(2, Dice.d, 8); // rolls two d8
+  // Dice.rollMany(3, Dice.d6_firepower); // rolls 3 d6_firepower
+  rollMany(n, diceFunc = null, sides = 6) {
     const rolls = [];
     for (let i = 0; i < n; i++) {
-      rolls.push(this.d(sides));
+      if (diceFunc) {
+        rolls.push(diceFunc.call(this));
+      } else {
+        rolls.push(this.d(sides));
+      }
     }
     return rolls;
   }
